ab9210fd3968d31faa51bf046e175df41ca2ea02
Nikhilll-dev-code <sathellinikhil@gmail.com>
2026-02-06 11:51:28 +0530

    chore: restructuring repo to proper root format

--- src/components/TempoSlider.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-const TempoSlider = ({ value, onChange }) => (
-  <div className="control-group">
-    <label>Tempo: {value} BPM</label>
-    <input type="range" min="60" max="200" value={value} onChange={(e) => onChange(e.target.value)} className="modern-slider" style={{width: '100%'}} />
-  </div>
-);
-export default TempoSlider;
